c9849e3234b28b60380f7a17935352be3220b21b
Ergonomisk <erik.ovukaandersson@elev.ga.ntig.se>
2026-02-26 16:22:59 +0100

    Added a way to loggin and playing a adventure

--- app.rb
+++ app.rb
@@ -4,21 +4,81 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 
-get ('/home') do
-  db=SQLite3::Database.new('db/databas.db')
-  db.results_as_hash = true
-  @databasadventurename = db.execute("SELECT * FROM adventurename")
-  p @databasadventurename
-  slim(:"index")
+
+get('/') do
+  
+  slim(:loggin)
 
 end
 
-get ('/adventure/:id') do
+get('/home') do
+  db=SQLite3::Database.new('db/databas.db')
+  db.results_as_hash = true
+  @databasadventurename = db.execute("SELECT * FROM adventurename")
+  slim(:"index")
+end
+
+get('/adventure/:id') do
   db=SQLite3::Database.new('db/databas.db')
   db.results_as_hash = true
   id = params[:id].to_i
-  @special_adventure = db.execute("SELECT * FROM adventurename WHERE id = ?",id).first
+
+  @special_adventure = db.execute("SELECT * FROM adventurename WHERE id = ?", id).first
+  @rooms = db.execute("SELECT * FROM arooms WHERE adventure_id = ?", id)
+
+  # Get actions for all rooms in this adventure
+  @actions_by_room = {}
+  @rooms.each do |room|
+    @actions_by_room[room["id"]] = db.execute("SELECT * FROM actions WHERE room_id = ?", room["id"])
+  end
+
   slim(:'adventure')
-  
+end
+
+
+post('/login')do
+  name = params["name"]
+  pwd = params["pwd"]
+  db = SQLite3::Database.new("db/databas.db")
+  db.results_as_hash = true
+  result=db.execute("SELECT id,pwd_digest FROM user WHERE name=?", name)
+  if result.empty?
+    redirect('/error')
+  end
+  user_id = (result.first["id"])
+  pwd_digest =result.first["pwd_digest"]
+  if BCrypt::Password.new(pwd_digest) == pwd
+    session[:user_id] = user_id
+    redirect('/home')
+  else
+    redirect('/')
+  end
+
+end
+
+
+post('/user') do
+  name = params["name"]
+  pwd = params["pwd"]
+  pwd_confirm = params["pwd_confirm"]
+
+  if pwd.length < 3
+    redirect('/?error=Lösenordet+måste+vara+minst+3+tecken')
+  end
+
+  db = SQLite3::Database.new("db/databas.db")
+  result=db.execute("SELECT id FROM user WHERE name=?", name)
+
+  if result.empty?
+    if pwd == pwd_confirm
+      pwd_digest=BCrypt::Password.create(pwd)
+      db.execute("INSERT INTO user(name, pwd_digest) VALUES(?,?)",[name, pwd_digest])
+      redirect('/home')
+    else
+      redirect('/?error=Lösenorden+matchar+inte')
+    end
+  else
+    redirect('/?error=Användarnamnet+är+redan+taget')
+  end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -2,9 +2,7 @@ require 'sqlite3'
 
 db = SQLite3::Database.new("databas.db")
 
-
 def seed!(db)
-  puts "Using db file: db/databas.db"
   puts "🧹 Dropping old tables..."
   drop_tables(db)
   puts "🧱 Creating tables..."
@@ -19,40 +17,59 @@ def drop_tables(db)
   db.execute('DROP TABLE IF EXISTS user')
   db.execute('DROP TABLE IF EXISTS arooms')
   db.execute('DROP TABLE IF EXISTS actions')
+  db.execute('DROP TABLE IF EXISTS adventure_rooms')
+  db.execute('DROP TABLE IF EXISTS room_actions')
 end
-
 
 def create_tables(db)
   db.execute('CREATE TABLE adventurename (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               pic_link TEXT,
-              name TEXT NOT NULL, 
+              name TEXT NOT NULL,
               description TEXT)')
+
   db.execute('CREATE TABLE user (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL, 
+              name TEXT NOT NULL,
               pwd_digest TEXT NOT NULL)')
+
   db.execute('CREATE TABLE arooms (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL, 
-              description TEXT)')
+              adventure_id INTEGER NOT NULL,
+              name TEXT NOT NULL,
+              description TEXT,
+              FOREIGN KEY (adventure_id) REFERENCES adventurename(id))')
+
   db.execute('CREATE TABLE actions (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL, 
-              description TEXT)')
-  
+              room_id INTEGER NOT NULL,
+              name TEXT NOT NULL,
+              result TEXT,
+              FOREIGN KEY (room_id) REFERENCES arooms(id))')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO adventurename (name, description) VALUES ("Äventyr 1", "jsdfjnkvcxbjifvbnjkvcxjb")')
-  db.execute('INSERT INTO adventurename (name, description) VALUES ("Äventyr 2", "EFDVÖKOJVCDHPJFVDHJKDCVNJK")')
-  db.execute('INSERT INTO adventurename (name, description) VALUES ("Äventyr 3", "jixidfivnj3984ieoppdoododjvglkjcvjkcxk")')
+  # Adventures
+  db.execute('INSERT INTO adventurename (name, description) VALUES ("Äventyr 1", "En mörk skog fylld med mysterier.")')
+  db.execute('INSERT INTO adventurename (name, description) VALUES ("Äventyr 2", "Ett gammalt slott med hemliga rum.")')
+  db.execute('INSERT INTO adventurename (name, description) VALUES ("Äventyr 3", "En underjordisk grotta med skatter.")')
+
+  # Rooms for Adventure 1 (id=1)
+  db.execute('INSERT INTO arooms (adventure_id, name, description) VALUES (1, "Skogens ingång", "Du står vid kanten av en tät skog. Det luktar mossa och fuktig jord.")')
+  db.execute('INSERT INTO arooms (adventure_id, name, description) VALUES (1, "Det gamla trädet", "Ett enormt gammalt träd tornar upp sig framför dig. Äpplen hänger i grenarna.")')
+
+  # Rooms for Adventure 2 (id=2)
+  db.execute('INSERT INTO arooms (adventure_id, name, description) VALUES (2, "Slottets port", "En massiv järnport blockerar ingången. Den är lätt på glänt.")')
+  db.execute('INSERT INTO arooms (adventure_id, name, description) VALUES (2, "Stora salen", "En dammig sal med rustningar längs väggarna och ett stort bord i mitten.")')
+
+  # Actions for room 1 (Skogens ingång)
+  db.execute('INSERT INTO actions (room_id, name, result) VALUES (1, "Gå in i skogen", "Du kliver in bland de täta träden...")')
+  db.execute('INSERT INTO actions (room_id, name, result) VALUES (1, "Titta runt", "Du ser ett spår i marken som leder norrut.")')
+
+  # Actions for room 2 (Det gamla trädet)
+  db.execute('INSERT INTO actions (room_id, name, result) VALUES (2, "Plocka ett äpple", "Du sträcker dig upp och tar ett saftigt äpple. +1 HP!")')
+  db.execute('INSERT INTO actions (room_id, name, result) VALUES (2, "Slå på trädet", "Du slår på trädet. Det händer ingenting. Din hand gör ont.")')
+  db.execute('INSERT INTO actions (room_id, name, result) VALUES (2, "Fråga trädet om ett äpple", "Trädet svarar inte. Men det känns som att det lyssnar.")')
 end
 
-
 seed!(db)
-
-
-
-
-
